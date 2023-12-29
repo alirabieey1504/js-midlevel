8bdee034bdcab89f6e8adf9a273c17a9117d72be
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-29 16:04:27 +0330

    Add files via upload

---
+++ leaen101.js
@@ -0,0 +1,12 @@
+let h1elemet = document.getElementById('title')
+console.log(h1elemet);
+let lielements = document.getElementsByClassName('list')
+console.log(lielements);
+console.log(lielements[1]);
+let listitem = document.getElementsByTagName('li')
+console.log(listitem[2]);
+let firstlistitem=document.querySelector('.list')
+console.log(firstlistitem);
+
+let uiItems=document.querySelectorAll('.list')
+console.log(uiItems);
