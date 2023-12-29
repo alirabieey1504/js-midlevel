db84cbb34ac4fc0b67d2aa4421ea9be6f0c6d3cb
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-29 18:54:57 +0330

    Add files via upload

---
+++ learn103.js
@@ -0,0 +1,9 @@
+
+let h1elem=document.getElementById('title')
+h1elem.style.color='gray'
+h1elem.style.fontSize='60px'
+let liitems=document.getElementsByClassName("list")
+let chane1=liitems[2].style.color='blue'
+let chane=liitems[2].style.fontWeight='bold'
+console.log(liitems);
+
