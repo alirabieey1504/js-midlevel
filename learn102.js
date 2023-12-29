320e2ff19cc8d96d4093015741d686a5aee91b2c
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-29 18:54:37 +0330

    Add files via upload

---
+++ learn102.js
@@ -0,0 +1,6 @@
+var list=document.getElementById('title')
+
+list.setAttribute('class','js-text')
+var listEl=document.getElementsByClassName('list')
+listEl[2].setAttribute('id','about')
+console.log(list.getAttribute("id"));
