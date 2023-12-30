0c6504f4a00ad1f1f089855e900ecb70520a92c8
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-31 00:16:27 +0330

    Add files via upload

---
+++ learn107.js
@@ -0,0 +1,12 @@
+let h1select = document.getElementById('title')
+function clickme() {
+    h1select.style.color='green'
+    alert('click shod :)')
+    
+}
+let qurebutton = document.querySelector('button')
+qurebutton.setAttribute('onclick', 'clickme()')
+
+
+
+
