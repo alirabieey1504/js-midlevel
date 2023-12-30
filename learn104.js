6ab30bef4ae692e2c53e2ca66696cf3c26907cb2
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-30 12:16:58 +0330

    Add files via upload

---
+++ learn104.js
@@ -0,0 +1,21 @@
+let h1elemet =document.getElementById('title')
+
+let h1s=document.querySelectorAll('.list')
+
+// console.log(h1s[1].innerHTML);
+// console.log(h1s[2].textContent);
+// console.log(h1s[0].innerText);
+let input=document.getElementById('input')
+let select=document.getElementById('select')
+console.log(input.value);
+select.value='tehran'
+input.value='react.js'
+console.log(select.value);
+
+h1s[0].innerHTML='درباره ما'
+
+
+let selected=document.getElementById('select')
+
+selected.Value='espanish'
+console.log(selected.value);
