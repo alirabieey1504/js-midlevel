f708c12c74307de3271150e845602376a4fa7f7f
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2024-01-02 13:15:10 +0330

    Add files via upload

---
+++ learn111.js
@@ -0,0 +1,42 @@
+// function keylog() {
+//     console.log('tiped');
+// }
+
+// let h1elem = document.getElementById('title')
+
+// h1elem.addEventListener('click', function () {
+//     // // h1elem.setAttribute('class','red')
+//     // h1elem.classList.add('red')
+//     // h1elem.classList.remove('font-sm')
+//     h1elem.classList.toggle('red') 
+// })
+
+
+//onchange ('')
+
+// let onch=document.getElementById('select')
+
+
+// function changehandlre(){
+//     console.log('change');
+
+//  console.log(onch.innerHTML);
+// }
+
+
+// let property=document.getElementById('title')
+
+// // console.log(property.getAttribute('class'));
+
+// console.log(property.className);
+
+// document.addEventListener("DOMContentLoaded", function(event) {
+    // let inputuser=document.getElementById('inputd')
+    // console.log(inputuser.getAttribute('user-type'));
+// });
+
+let inputdelem=document.getElementById('inputd')
+inputdelem.usertype='adminffff'
+console.log(inputdelem.usertype);
+console.log(inputdelem.getAttribute('usertype'));
+
